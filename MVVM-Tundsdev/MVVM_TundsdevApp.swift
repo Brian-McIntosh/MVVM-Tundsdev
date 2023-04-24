24b5a72dcad6f1fb82a57ed99f4c93732198f65c
Brian McIntosh <brian@mixinteractive.com>
2023-04-23 22:41:26 -0400

    Initial Commit

---
+++ MVVM-Tundsdev/MVVM_TundsdevApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_TundsdevApp.swift
+//  MVVM-Tundsdev
+//
+//  Created by Brian McIntosh on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_TundsdevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
